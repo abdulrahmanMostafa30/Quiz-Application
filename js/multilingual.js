d98caf04827b43f85759ad966ede62264a98db02
Abdulrahman Mostafa <3bdo.mostafa@protonmail.com>
2023-04-10 15:40:57 +0200

    commit

--- js/multilingual.js
+++ js/multilingual.js
@@ -27,7 +27,6 @@ var setLanguage = (language) => {
   });
   document.dir = language === "ar" ? "rtl" : "ltr";
 };
-
 Object.keys(localStorage).forEach(function (key) {
   if (key != 'lang' || key != 'theme') {
     try {
@@ -38,11 +37,12 @@ Object.keys(localStorage).forEach(function (key) {
         document.getElementById('LogInNav').setAttribute('data-i18n', 'logout')
         document.getElementById('LogInNav').href = "#";
         loginDone = true
-        if (window.location.pathname.includes('index.html')) {
+        if (window.location.pathname.indexOf('index.html')) {
           document.getElementById('SignUpHome').setAttribute('data-i18n', 'quizes')
           document.getElementById('SignUpHome').href = 'dashboard.html'
         }
-        if (window.location.pathname.includes('dashboard.html')) {
+        if (window.location.pathname.indexOf('dashboard.html')) {
+          alert('dashboard')
           document.getElementById('uName').textContent = userFromLocalStorage.name
         }
         return
@@ -53,10 +53,12 @@ Object.keys(localStorage).forEach(function (key) {
   }
 });
 if (!loginDone) {
-  if (window.location.pathname == '/dashboard.html') {
+  if (window.location.pathname.indexOf('dashboard.html')) {
     document.getElementById('uName').setAttribute('data-i18n', 'anonymous')
   }
 }
+
+
 
 
 $(window).on('load', function () {
